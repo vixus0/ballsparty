9e3a1c351c6909268ff4c05dd3abeca95e24eac3
Anshul Sirur <vixus0@gmail.com>
2020-04-11 00:41:01 +0200

    ball instances

--- ball.gd
+++ ball.gd
@@ -6,12 +6,12 @@ extends Node2D
 # var b = "text"
 
 var waypoint = Vector2()
+var player_name = "None"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.waypoint = self.get_global_position()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- root.gd
+++ root.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
+const BALL = preload("res://ball.tscn")
+
 var my_ip
 var menu
 var peer
 var connected = false
+var me
+
+# Player info, associate ID to data
+var player_info = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	peer = NetworkedMultiplayerENet.new()
 	my_ip = IP.get_local_addresses()[0]
-	menu = get_node("menu/grid")
-	get_node("menu").pause_mode = Node.PAUSE_MODE_PROCESS
+	menu = get_node("static/menu/grid")
+	get_node("static/menu/lbl_myip_value").text = my_ip
+	menu.pause_mode = Node.PAUSE_MODE_PROCESS
 	get_tree().paused = true
 # warning-ignore:return_value_discarded
 	get_tree().connect("connected_to_server", self, "_connected_ok")
@@ -18,19 +25,30 @@ func _ready():
 	get_tree().connect("connection_failed", self, "_connected_fail")
 # warning-ignore:return_value_discarded
 	get_tree().connect("server_disconnected", self, "_server_disconnected")
+# warning-ignore:return_value_discarded
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+# warning-ignore:return_value_discarded
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+
 
 func _connected_ok():
 	connected = true
-	get_node("menu").visible = false
+	menu.visible = false
 	get_tree().paused = false
+	_create_player(name)
 
 func _connected_fail():
-	get_node("menu").visible = true
+	menu.visible = true
 	get_tree().paused = true
 
 func _server_disconnected():
-	get_node("menu").visible = true
+	menu.visible = true
 	get_tree().paused = true
+	
+func _create_player(host_name):
+	me = BALL.instance()
+	me.player_name = host_name
+	add_child(me)
 
 func _on_btn_connect_pressed():
 	var name = menu.get_node("edit_name").text
@@ -40,7 +58,6 @@ func _on_btn_connect_pressed():
 	peer.create_client(server_ip, server_port)
 	get_tree().set_network_peer(peer)
 
-
 func _on_btn_host_pressed():
 	var name = menu.get_node("edit_name").text
 	var server_port = int(menu.get_node("edit_port").text)
@@ -49,3 +66,23 @@ func _on_btn_host_pressed():
 	peer.create_server(server_port, max_players)
 	get_tree().set_network_peer(peer)
 	_connected_ok()
+	
+func _player_connected(id):
+	# Called on both clients and server when a peer connects. Send my info to it.
+	rpc_id(id, "register_player", {name = me.player_name, pos = me.get_global_position()})
+	print("connected ", id)
+	print("info ", player_info)
+
+func _player_disconnected(id):
+	player_info.erase(id) # Erase player from info.
+	print("disconnected ", id)
+
+remote func register_player(info):
+	# Get the id of the RPC sender.
+	var id = get_tree().get_rpc_sender_id()
+	# Store the info
+	var ball = BALL.instance()
+	ball.player_name = info.name
+	ball.set_global_position(info.pos)
+	player_info[id] = ball
+	print("register ", id, info)
