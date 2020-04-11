0de78347fe0dc60eaf72192fac5f0dbe120d74d4
Anshul Sirur <vixus0@gmail.com>
2020-04-11 02:22:46 +0200

    jiggling balls

--- ball.gd
+++ ball.gd
@@ -1,9 +1,6 @@
-extends Node2D
+extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const IMPULSE = 160.0
 
 var waypoint = Vector2()
 var player_name = "None"
@@ -12,17 +9,18 @@ var current_player = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.waypoint = self.get_global_position()
+	waypoint = get_position()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var dp = self.get_global_position() - self.waypoint
-	if dp.length() > 1:
-		self.set_global_position(self.get_global_position() - dp * 5 * delta)	
+	var dp = waypoint - get_position()
+	if dp.length() < 5:
+		return
+	apply_impulse(Vector2(0,0), dp.normalized() * IMPULSE)
 	
 func _input(event):
 	if current_player and (event is InputEventMouseButton and event.button_index == BUTTON_LEFT):
-		self.waypoint = get_global_mouse_position()
+		waypoint = get_global_mouse_position()
 
 func set_current():
 	current_player = true
